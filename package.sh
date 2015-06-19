d79542c8e90e9b432f3208602a0ae8504a27bc4a
Reid Vandewiele <reid@puppetlabs.com>
2015-06-19 14:26:12 -0700

    Add support for running package on osx
    Per the comment in the script, this commit adds support for running
    `pacakge.sh` on either Linux or OSX. This allows for manual builds in
    the event that a manual build is required.

--- package.sh
+++ package.sh
@@ -4,7 +4,7 @@
 # checked out modules) and place it in pkg/. The resulting artifact can be
 # uploaded to a file hosting service and fetched on target nodes.
 
-r10k puppetfile install
+r10k puppetfile install -v
 
 working_dir=$(basename $(cd $(dirname $0) && pwd))
 containing_dir=$(cd $(dirname $0)/.. && pwd)
@@ -15,7 +15,19 @@ echo $basename
 version=$1
 versioned_name="seteam-production-${version}"
 mkdir -p "${basename}/pkg"
+
+# Because engineer laptops run OSX and build systems run Linux, this script
+# should work correctly on both. Because BSD tar accepts different flags from
+# GNU tar for some of the more complex operations, we need to define logic to
+# enable that.
+if [[ "$(uname)" = 'Darwin' ]]; then
+  platform_tar_flags="-s '/${working_dir}/${versioned_name}/' --disable-copyfile"
+else
+  platform_tar_flags="--transform='s/${working_dir}/${versioned_name}/'"
+fi
+
 tar \
+  $platform_tar_flags \
   -C "${containing_dir}" \
   --exclude ".git" \
   --exclude ".gitignore" \
@@ -25,7 +37,6 @@ tar \
   --exclude "${working_dir}/pkg" \
   --exclude "${working_dir}/pkg/*" \
   --exclude "${working_dir}/package.sh" \
-  --transform="s/${working_dir}/${versioned_name}/" \
   -cvzf "${containing_dir}/${versioned_name}.tar.gz" \
   "${working_dir}"
 mv "${containing_dir}/${versioned_name}.tar.gz" "${basename}/pkg"
