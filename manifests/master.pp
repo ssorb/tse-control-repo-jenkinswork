e6cc441fc1059f172fb81da54ede57d69b32efd3
Reid Vandewiele <reid@puppetlabs.com>
2015-07-17 12:34:20 -0700

    Update r10k remote parameter
    This change is being made in prep for a new build process wherein the
    Puppet Enterprise master will have a local copy of all repositories in
    /opt/puppet/repos.
    
    This is the next step up. Previously the r10k configuration was laid
    down but not used except in unusual circumstances, now the r10k config
    will be self-contained and local but fully functional on a new master.
    
    Also remove the empty parameter list from the role to better indicate
    that roles should not take parameters.
    
    This seems like it should probably be a profile...

--- manifests/master.pp
+++ manifests/master.pp
@@ -2,8 +2,8 @@
 # Amazon, and generally enables SE Team specific patterns dependent on master
 # capabilities.
 #
-class role::master (
-) {
+class role::master {
+
   # Detect Vagrant
   $srv_root = $::virtual ? {
     'virtualbox' => '/var/seteam-files',
@@ -63,7 +63,7 @@ class role::master (
     line => 'github.com,192.30.252.130 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==',
   }->
   class { 'r10k':
-    remote     => 'git@github.com:puppetlabs/seteam-puppet-environments.git',
+    remote     => '/opt/puppet/repos/puppet-control.git',
     provider   => 'pe_gem',
     configfile => '/etc/puppetlabs/r10k/r10k.yaml',
   }
