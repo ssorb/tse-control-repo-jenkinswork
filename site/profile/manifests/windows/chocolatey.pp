6db27846bba3fe563047b0ac0aae01e7a541f2e4
9bryan <bryan.wood@puppetlabs.com>
2015-09-30 07:58:25 -0700

    Update chocolatey.pp so 2k8 uses special media and 2k12 doesnt
    This is a hack and should be removed once the chocolatey module/media work on 2k8 and 2k12.

--- site/profile/manifests/windows/chocolatey.pp
+++ site/profile/manifests/windows/chocolatey.pp
@@ -1,6 +1,15 @@
 class profile::windows::chocolatey (
-  $chocolatey_download_url = undef,
+  #$chocolatey_download_url = undef,
 ) {
+
+  case $::operatingsystemrelease {
+    '2008 R2': {
+      $chocolatey_download_url = 'https://s3-us-west-2.amazonaws.com/tse-builds/misc/chocolatey.0.9.9.9.nupkg'
+    }
+    default  : {
+      $chocolatey_download_url = undef
+    }
+  }
 
   class { 'chocolatey':
     chocolatey_download_url => $chocolatey_download_url,
