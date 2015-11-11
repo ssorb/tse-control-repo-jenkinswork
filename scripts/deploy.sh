0baf542bfff38aa94cae8df1aae5696c222063e2
Reid Vandewiele <reid@puppetlabs.com>
2015-11-11 15:50:10 -0800

    Revert to puppet-apply then puppet-agent
    We keep too much classification for the Master in the PE Console.
    Because of this, it's not elegant to try to fit everything into one
    single puppet-apply run. Therefore, minimize the puppet-apply to the
    smallest possible bootstrap configuration (set up the node manager),
    then run puppet-agent to do the rest.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,14 +11,11 @@ main()
 {
   sync_environment_contents
 
-  # Bootstrap classification by using puppet-apply to configure the node manager.
-  # Specify the classifier node_terminus to puppet-apply in order to include not
-  # just role::master, but also default Puppet Enterprise classification in the
-  # run.
+  # Bootstrap classification by using puppet-apply to configure the node
+  # manager, then running puppet-agent to enforce all configured state.
   /bin/bash $tools_path/refresh_classes.sh
-  puppet_suppress_storeconfig_warnings apply \
-    --node_terminus classifier \
-    --exec 'include role::master'
+  puppet apply --exec 'include profile::master::node_manager'
+  puppet agent --onetime --no-daemonize --color=false --verbose
 
   # We don't yet have the RBAC Directory Service puppetized so we have to
   # configure it separately. Sadness.
@@ -44,21 +41,6 @@ sync_environment_contents()
     echo "Syncing environment contents from $containing_dir to $puppet_environmentpath/production/"
     /bin/cp -Rfu "$containing_dir/"* "$puppet_environmentpath/production/"
   fi
-}
-
-puppet_suppress_storeconfig_warnings()
-{
-  # Because we use exported resources, there are a number of expected warnings
-  # which we will selectively suppress. This ends up looking a little
-  # complicated due to the need to preserve the success/failure semantics of
-  # the exit code emitted by the puppet invocation.
-  { puppet "$@" 2>&1 >&3 \
-    | sed \
-      -e '/Warning: You cannot collect without storeconfigs being set/d' \
-      -e '/Warning: You cannot collect exported resources without storeconfigs being set/d' \
-      -e '/Warning: Not collecting exported resources without storeconfigs/d' \
-    ; rc=${PIPESTATUS[0]}; test ${rc} -eq 0 || test ${rc} -eq 2 \
-  ; } 3>&1
 }
 
 main "$@"
