de89374aad5996559d2ac392acaebd93aef55c96
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2015-12-30 22:23:55 +1100

    Added cookie to acceot the license

--- site/profile/manifests/master/files/jdk.pp
+++ site/profile/manifests/master/files/jdk.pp
@@ -9,6 +9,10 @@ class profile::master::files::jdk (
   remote_file { 'jdk-8u45-windows-x64.exe':
     source  => 'http://download.oracle.com/otn-pub/java/jdk/8u45-b15/jdk-8u45-windows-x64.exe',
     path    => "${srv_root}/jdk/jdk-8u45-windows-x64.exe",
+    headers => {
+      'user-agent' => 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko',
+      'Cookie'     => 'oraclelicense=accept-securebackup-cookie;gpw_e24=http://edelivery.oracle.com'
+    }, # Oracle makes you accept the license agreement -_-
     require => File["${srv_root}/jdk"],
   }
 }
