0155284d7c9a7f09257c361f466ccd7784854294
Bryan Wood <bryan.wood@puppetlabs.com>
2016-03-25 09:32:15 -0700

    Initialize and setup admin user

--- site/profile/manifests/gitlab.pp
+++ site/profile/manifests/gitlab.pp
@@ -43,4 +43,18 @@ class profile::gitlab {
     require      => File["/etc/gitlab/ssl/${::fqdn}.key", "/etc/gitlab/ssl/${::fqdn}.key"],
   }
 
+  #Initialize gitlab
+  file { '/etc/gitlab/init.sh':
+    ensure  => file,
+    mode    => '0700',
+    owner   => root,
+    group   => root,
+    source  => 'puppet:///modules/profile/gitlab-init.sh',
+    require => Class['gitlab'],
+  }
+  exec { '/etc/gitlab/init.sh && touch /etc/gitlab/init':
+    creates => '/etc/gitlab/init',
+    require => File['/etc/gitlab/init.sh'],
+  }
+
 }
