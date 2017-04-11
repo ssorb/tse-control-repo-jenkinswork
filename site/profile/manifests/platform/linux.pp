6b2346754e2fbaf5f15addf7c1b04c8bf78e2fb4
SeanTecha <sean.t@puppet.com>
2017-04-11 11:41:51 -0700

    Update linux.pp

--- site/profile/manifests/platform/linux.pp
+++ site/profile/manifests/platform/linux.pp
@@ -1,7 +1,7 @@
 class profile::platform::linux {
 
   include profile::platform::linux::motd
-  include profile::platform::linux::vim
+  #include profile::platform::linux::vim
   include profile::platform::linux::ssh
   #include profile::baseline::linux::zsh
   include profile::platform::linux::user
