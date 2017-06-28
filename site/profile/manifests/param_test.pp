bb57fa6763acecfda8efe8b98ad243f539e81c9d
Administrator <admin@example.com>
2017-06-28 21:19:19 +0000

    Add new file

---
+++ site/profile/manifests/param_test.pp
@@ -0,0 +1,10 @@
+# param_test
+class profile::param_test (
+  Optional[String] $splunk_server = undef,
+){
+  if $splunk_server == undef {
+    $splunk_nodes_query = 'resources[certname] { type = "Class" and title = "Apache" }'
+    $splunk_server = puppetdb_query($splunk_nodes_query)[0][certname]
+  }
+  notify { "This is my test value: $splunk_server": }
+}
