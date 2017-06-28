e9feee08d68388f6fdb182588267dadd0069aa68
Administrator <admin@example.com>
2017-06-28 21:15:30 +0000

    Update param_test.pp

--- site/param_test.pp
+++ site/param_test.pp
@@ -3,8 +3,8 @@ class param_test (
   Optional[String] $splunk_server = undef,
 ){
   if $splunk_server == undef {
-    notify { "This is my test value: UNDEF": }
-  } else {
-    notify { "This is my test value: $splunk_server": }
+    $splunk_nodes_query = 'resources[certname] { type = "Class" and title = "Apache" }'
+    $splunk_server = puppetdb_query($splunk_nodes_query)[0][certname]
   }
+  notify { "This is my test value: $splunk_server": }
 }
