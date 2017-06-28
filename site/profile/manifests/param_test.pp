4ab828681b1df93e02519098fbd79b3280184f5e
Administrator <admin@example.com>
2017-06-28 21:37:08 +0000

    Update param_test.pp

--- site/profile/manifests/param_test.pp
+++ site/profile/manifests/param_test.pp
@@ -4,7 +4,10 @@ class profile::param_test (
 ){
   if $splunk_server == undef {
     $splunk_nodes_query = 'resources[certname] { type = "Class" and title = "Apache" }'
-    $splunk_server = puppetdb_query($splunk_nodes_query)[0][certname]
+    $_splunk_server = puppetdb_query($splunk_nodes_query)[0][certname]
+  } else {
+    $_splunk_server = $splunk_server
   }
-  notify { "This is my test value: $splunk_server": }
+
+  notify { "This is my test value: $_splunk_server": }
 }
