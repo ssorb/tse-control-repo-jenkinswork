6fd7d3c5d145b59ed9dc74b890bf0c6d5f0107fa
Administrator <admin@example.com>
2017-07-26 21:00:17 +0000

    Update server.pp

--- site/profile/manifests/jenkins/server.pp
+++ site/profile/manifests/jenkins/server.pp
@@ -1,9 +1,18 @@
 # Requires rtyler/jenkins module
 class profile::jenkins::server {
 
+  java::oracle { 'jdk8' :
+    ensure        => 'present',
+    url_hash      => 'd54c1d3a095b4ff2b6607d096fa80163',
+    version_major => '8u131',
+    version_minor => 'b11',
+    java_se       => 'jdk',
+  }
+
   class { 'jenkins':
     configure_firewall => true,
     direct_download    => 'http://pkg.jenkins-ci.org/redhat-stable/jenkins-2.7.4-1.1.noarch.rpm',
+    require            => Java::Oracle['jdk8'],
   }
 
   jenkins::user { 'root':
