c7a091d584ef8ee511bb87702a0ad7bf953f8057
Administrator <admin@example.com>
2017-07-27 21:10:57 +0000

    Update server.pp

--- site/profile/manifests/jenkins/server.pp
+++ site/profile/manifests/jenkins/server.pp
@@ -35,7 +35,7 @@ class profile::jenkins::server {
     #cleanup       => true, # Do not use this argument with this workaround for idempotency reasons
     extract       => true,
     extract_path  => $jenkins_path,
-    creates       => "${jenkins_path}/config.xml" #directory inside tgz
+    creates       => "${jenkins_path}/config.xml", #directory inside tgz
     require       => [ File[$docs_gz_path],Class['jenkins'] ],
   }
 
