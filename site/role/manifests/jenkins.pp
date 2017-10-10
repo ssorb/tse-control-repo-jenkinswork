5f1105e8a593b2c51cee9cfb49cecd8f973f6c03
Abir Majumdar <beersy@gmail.com>
2017-10-10 11:53:39 -0700

    Update jenkins.pp

--- site/role/manifests/jenkins.pp
+++ site/role/manifests/jenkins.pp
@@ -1,11 +1,8 @@
 class role::jenkins {
 
   class { 'profile::jenkins::server' :
-   gms_api_token      => 'et9FqxWxzkoF7GJXgqTQ',
+#   gms_api_token      => 'et9FqxWxzkoF7GJXgqTQ',
    gitlab_domain      => 'gitlab.inf.puppet.vm',  
-   gitlab_ip          => '192.168.0.95',   
-   prod_deploy_domain => 'javaappserver.puppet.pdx.vm',  
-   prod_deploy_ip     => '192.168.0.102',         
   }
   
   include profile::jenkins::jobs
